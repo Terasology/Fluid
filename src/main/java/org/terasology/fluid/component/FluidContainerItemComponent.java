469df042213b86e75f6f0991c6848233a6acb4f2
DarkWeird <foxflameless@gmail.com>
2021-07-09 16:53:19 +0300

    fix(ecs-gestalt): Migrate Components to gestalt's Components.

--- src/main/java/org/terasology/fluid/component/FluidContainerItemComponent.java
+++ src/main/java/org/terasology/fluid/component/FluidContainerItemComponent.java
@@ -1,30 +1,17 @@
-/*
- * Copyright 2015 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.fluid.component;
 
 import org.joml.Vector2f;
-import org.terasology.engine.entitySystem.Component;
 import org.terasology.engine.network.Replicate;
 import org.terasology.engine.rendering.assets.texture.TextureRegionAsset;
+import org.terasology.gestalt.entitysystem.component.Component;
 import org.terasology.module.inventory.components.ItemDifferentiating;
 
 /**
  * This component indicates that an entity is a container capable of holding fluids.
  */
-public class FluidContainerItemComponent implements Component, ItemDifferentiating {
+public class FluidContainerItemComponent implements Component<FluidContainerItemComponent>, ItemDifferentiating {
 
     /** The type of the fluid it can contain */
     @Replicate
@@ -85,5 +72,16 @@ public class FluidContainerItemComponent implements Component, ItemDifferentiati
         }
 
         return true;
+    }
+
+    @Override
+    public void copy(FluidContainerItemComponent other) {
+        this.fluidType = other.fluidType;
+        this.volume = other.volume;
+        this.maxVolume = other.maxVolume;
+        this.fluidMinPerc = new Vector2f(other.fluidMinPerc);
+        this.fluidSizePerc = new Vector2f(other.fluidSizePerc);
+        this.textureWithHole = other.textureWithHole;
+        this.emptyTexture = other.emptyTexture;
     }
 }
--- src/main/java/org/terasology/fluid/component/FluidInventoryComponent.java
+++ src/main/java/org/terasology/fluid/component/FluidInventoryComponent.java
@@ -1,32 +1,19 @@
-/*
- * Copyright 2014 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.fluid.component;
 
 import com.google.common.collect.Lists;
-import org.terasology.engine.entitySystem.Component;
 import org.terasology.engine.entitySystem.Owns;
 import org.terasology.engine.entitySystem.entity.EntityRef;
 import org.terasology.engine.network.Replicate;
+import org.terasology.gestalt.entitysystem.component.Component;
 
 import java.util.List;
 
 /**
  * Represents the fluid inventory of an entity which the entity uses to store fluids.
  */
-public class FluidInventoryComponent implements Component {
+public class FluidInventoryComponent implements Component<FluidInventoryComponent> {
 
     /** A list of fluid slots which fluids can occupy */
     @Replicate
@@ -54,5 +41,11 @@ public class FluidInventoryComponent implements Component {
             fluidSlots.add(EntityRef.NULL);
             maximumVolumes.add(maximumVolume);
         }
+    }
+
+    @Override
+    public void copy(FluidInventoryComponent other) {
+        this.fluidSlots = Lists.newLinkedList(this.fluidSlots);
+        this.maximumVolumes = Lists.newLinkedList(maximumVolumes);
     }
 }
