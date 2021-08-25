f49230b6300902db5c3da595e5c93394e7c3018c
Nail Khanipov <foxflameless@gmail.com>
2021-08-25 16:45:56 +0300

    fix(ecs-gestalt): Migrate Components to gestalt's Components. (#35)
    Ref: MovingBlocks/Terasology#4753
    
    Co-authored-by: Tobias Nett <skaldarnar@googlemail.com>

--- src/main/java/org/terasology/fluid/component/FluidComponent.java
+++ src/main/java/org/terasology/fluid/component/FluidComponent.java
@@ -1,27 +1,14 @@
-/*
- * Copyright 2014 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.fluid.component;
 
-import org.terasology.engine.entitySystem.Component;
 import org.terasology.engine.network.Replicate;
+import org.terasology.gestalt.entitysystem.component.Component;
 
 /**
  * This component indicates that an entity is a fluid, and contains basic attributes of the fluid.
  */
-public class FluidComponent implements Component {
+public class FluidComponent implements Component<FluidComponent> {
     /** The type of the fluid */
     @Replicate
     public String fluidType;
@@ -29,4 +16,10 @@ public class FluidComponent implements Component {
     /** The volume of the fluid */
     @Replicate
     public float volume;
+
+    @Override
+    public void copyFrom(FluidComponent other) {
+        this.fluidType = other.fluidType;
+        this.volume = other.volume;
+    }
 }
